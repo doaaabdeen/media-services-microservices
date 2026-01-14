ef0257370933ae181d0ae0bbcb1541095564b27f
doaaabdeen <dodo.abd2005@gmail.com>
2026-01-14 15:26:31 +0200

    Update Product.java

--- product-service/src/main/java/com/media/productservice/Product.java
+++ product-service/src/main/java/com/media/productservice/Product.java
@@ -5,20 +5,21 @@ public class Product {
     private String name;
     private double price;
 
-    // هذا الجزء يسمى Constructor (لبناء المنتج)
+    // This part is called a Constructor (for creating the product)
     public Product(String id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
     }
 
-    // هذه تسمى Getters (لكي يتمكن الـ Gateway من قراءة البيانات وعرضها)
+  // These are called Getters (so that the Gateway can read and display the data)
     public String getId() { return id; }
     public String getName() { return name; }
     public double getPrice() { return price; }
 
-    // هذه تسمى Setters (لكي نتمكن من تعديل البيانات)
+  // These are called Setters (to allow us to modify the data)
     public void setId(String id) { this.id = id; }
     public void setName(String name) { this.name = name; }
     public void setPrice(double price) { this.price = price; }
+
 }
